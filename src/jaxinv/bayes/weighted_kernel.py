79ecf643b08a4961af8346b71bcc0639e30812d0
Hajime Kawahara <divrot@gmail.com>
2025-04-19 11:52:21 +0900

    GKG type 4

--- src/jaxinv/bayes/weighted_kernel.py
+++ src/jaxinv/bayes/weighted_kernel.py
@@ -53,3 +53,29 @@ def generalized_weighted_kernel_type3(
         * kernel_model_t
         * (geometric_weight @ kernel_model_s @ geometric_weight.T)
     )
+
+
+def generalized_weighted_kernel_type4(
+    geometric_weight,
+    spectral_matrix,
+    kernel_model_s,
+    kernel_model_t,
+    kernel_model_x,
+    alpha,
+):
+    """computes the generalized weighted kernel of type 4
+
+    Args:
+        geometic_weight (2D array): geometric weight, W (Ni, Nj)
+        spectral_matrix (2D array): component -- spectrum matrix, X (Nk, Nl)
+        kernel_model_s (2D array): kernel for the model covariances for S(patial) (Nj, Nj)
+        kernel_model_t (2D array): kernel for the model covariances for T(emporal) (Ni, Ni)
+        kernel_model_x (2D array): kernel for the model covariances for K( components) (Nk, Nk)
+        alpha (float): intensity of the kernel
+
+    Returns:
+        2D array: generalized weighted kernel of type 4 (Ni, Ni)
+    """
+    XKX = spectral_matrix.T @ kernel_model_x @ spectral_matrix
+    WKW = geometric_weight @ kernel_model_s @ geometric_weight.T
+    return alpha * jnp.kron(WKW * kernel_model_t, XKX)
