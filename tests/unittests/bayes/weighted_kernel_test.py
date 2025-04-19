cbd93b4b01e51e8b961e493ee1deb9d87c08d49a
Hajime Kawahara <divrot@gmail.com>
2025-04-19 11:28:00 +0900

    GKG for type3 unittest added

--- tests/unittests/bayes/weighted_kernel_test.py
+++ tests/unittests/bayes/weighted_kernel_test.py
@@ -1,6 +1,7 @@
 from jaxinv.bayes.weighted_kernel import generalized_weighted_kernel_type1
 from jaxinv.bayes.weighted_kernel import generalized_weighted_kernel_type2
 from jaxinv.bayes.weighted_kernel import generalized_weighted_kernel_type3
+import pytest
 import jax.numpy as jnp
 from jax import random
 
@@ -16,15 +17,35 @@ def test_generalized_weighted_kernel_type1():
         result, expected_result
     ), f"Expected {expected_result}, got {result}"
 
+
 def test_generalized_weighted_kernel_type2():
     """Test the generalized weighted kernel of type 2"""
-    geometric_weight, spectral_matrix, kernel_model_s, _, kernel_model_x = _test_kernel_model(seed=0)
+    geometric_weight, spectral_matrix, kernel_model_s, _, kernel_model_x = (
+        _test_kernel_model(seed=0)
+    )
     alpha = 1.0
     result = generalized_weighted_kernel_type2(
-    geometric_weight, spectral_matrix, kernel_model_s, kernel_model_x, alpha)
+        geometric_weight, spectral_matrix, kernel_model_s, kernel_model_x, alpha
+    )
 
     assert result.shape == (10, 10), f"Expected shape (10, 10), got {result.shape}"
-    assert jnp.sum(result) == 106.90668, f"Expected sum 106.90668, got {jnp.sum(result)}"
+    assert (
+        jnp.sum(result) == pytest.approx(106.90668)
+    ), f"Expected sum 106.90668, got {jnp.sum(result)}"
+
+
+def test_generalized_weighted_kernel_type3():
+    """Test the generalized weighted kernel of type 3"""
+    geometric_weight, _, kernel_model_s, kernel_model_t, _ = _test_kernel_model(seed=0)
+    alpha = 1.0
+    result = generalized_weighted_kernel_type3(
+        geometric_weight, kernel_model_s, kernel_model_t, alpha
+    )
+    
+    
+    assert result.shape == (2, 2), f"Expected shape (2, 2), got {result.shape}"
+    assert jnp.sum(result) == pytest.approx(6.5702825), f"Expected sum 6.5702825, got {jnp.sum(result)}"
+
 
 def _test_kernel_model(Ni=2, Nj=3, Nk=4, Nl=5, seed=0):
     key = random.PRNGKey(seed)
@@ -33,10 +54,17 @@ def _test_kernel_model(Ni=2, Nj=3, Nk=4, Nl=5, seed=0):
     kernel_model_t = jnp.diag(random.uniform(key, shape=(Ni,)))
     kernel_model_s = jnp.diag(random.uniform(key, shape=(Nj,)))
     kernel_model_x = jnp.diag(random.uniform(key, shape=(Nk,)))
-    
-    return geometric_weight, spectral_matrix, kernel_model_s, kernel_model_t, kernel_model_x
+
+    return (
+        geometric_weight,
+        spectral_matrix,
+        kernel_model_s,
+        kernel_model_t,
+        kernel_model_x,
+    )
 
 
 if __name__ == "__main__":
     test_generalized_weighted_kernel_type1()
     test_generalized_weighted_kernel_type2()
+    test_generalized_weighted_kernel_type3()
