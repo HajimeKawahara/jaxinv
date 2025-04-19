0d343aec9eb7c10e2494bf19c3869b3de40e29ec
Hajime Kawahara <divrot@gmail.com>
2025-04-19 10:52:12 +0900

    unittest for GKG for type2

--- tests/unittests/bayes/weighted_kernel_test.py
+++ tests/unittests/bayes/weighted_kernel_test.py
@@ -4,20 +4,39 @@ from jaxinv.bayes.weighted_kernel import generalized_weighted_kernel_type3
 import jax.numpy as jnp
 from jax import random
 
+
 def test_generalized_weighted_kernel_type1():
     """Test the generalized weighted kernel of type 1"""
-    Ni = 2
-    Nj = 3
-    key = random.PRNGKey(0)
-    geometric_weight = random.normal(key, shape=(Ni, Nj))
-    kernel_model = jnp.diag(random.uniform(key, shape=(Nj,)))
+    geometric_weight, _, kernel_model, _, _ = _test_kernel_model(2, 3, seed=0)
 
     result = generalized_weighted_kernel_type1(geometric_weight, kernel_model)
-    expected_result = jnp.array([[6.571487,   0.36815816], [0.36815816, 0.35020235]])
+    expected_result = jnp.array([[6.571487, 0.36815816], [0.36815816, 0.35020235]])
 
-    assert jnp.allclose(result, expected_result), f"Expected {expected_result}, got {result}"
+    assert jnp.allclose(
+        result, expected_result
+    ), f"Expected {expected_result}, got {result}"
+
+def test_generalized_weighted_kernel_type2():
+    """Test the generalized weighted kernel of type 2"""
+    geometric_weight, spectral_matrix, kernel_model_s, _, kernel_model_x = _test_kernel_model(seed=0)
+    alpha = 1.0
+    result = generalized_weighted_kernel_type2(
+    geometric_weight, spectral_matrix, kernel_model_s, kernel_model_x, alpha)
+
+    assert result.shape == (10, 10), f"Expected shape (10, 10), got {result.shape}"
+    assert jnp.sum(result) == 106.90668, f"Expected sum 106.90668, got {jnp.sum(result)}"
+
+def _test_kernel_model(Ni=2, Nj=3, Nk=4, Nl=5, seed=0):
+    key = random.PRNGKey(seed)
+    geometric_weight = random.normal(key, shape=(Ni, Nj))
+    spectral_matrix = random.normal(key, shape=(Nk, Nl))
+    kernel_model_t = jnp.diag(random.uniform(key, shape=(Ni,)))
+    kernel_model_s = jnp.diag(random.uniform(key, shape=(Nj,)))
+    kernel_model_x = jnp.diag(random.uniform(key, shape=(Nk,)))
+    
+    return geometric_weight, spectral_matrix, kernel_model_s, kernel_model_t, kernel_model_x
 
 
 if __name__ == "__main__":
     test_generalized_weighted_kernel_type1()
-    
+    test_generalized_weighted_kernel_type2()
